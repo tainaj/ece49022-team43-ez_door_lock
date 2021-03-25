c0da795754a0f416d3752f498e326ee611684389
Joel Taina <jtaina124@gmail.com>
2021-03-25 17:11:46 -0400

    download profile from SD card successful

--- profile-recognition/components/SD-interface/SD-interface.c
+++ profile-recognition/components/SD-interface/SD-interface.c
@@ -94,49 +94,8 @@ void SD_init(void)
     // Use POSIX and C standard library functions to work with files.
     // First create a file.
     
-    /*ESP_LOGI(TAG, "Opening file");
-    FILE* f = fopen(MOUNT_POINT"/profiles/hello.txt", "w");
-    if (f == NULL) {
-        ESP_LOGE(TAG, "Failed to open file for writing");
-        return;
-    }
-    fprintf(f, "Hello %s!\n", card->cid.name);
-    fclose(f);
-    ESP_LOGI(TAG, "File written");
-
-    // Check if destination file exists before renaming
-    struct stat st;
-    if (stat(MOUNT_POINT"/profiles/foo.txt", &st) == 0) {
-        // Delete it if it exists
-        unlink(MOUNT_POINT"/profiles/foo.txt");
-    }
-
-    // Rename original file
-    ESP_LOGI(TAG, "Renaming file");
-    if (rename(MOUNT_POINT"/profiles/hello.txt", MOUNT_POINT"/profiles/foo.txt") != 0) {
-        ESP_LOGE(TAG, "Rename failed");
-        return;
-    }
-
-    // Open renamed file for reading
-    ESP_LOGI(TAG, "Reading file");
-    f = fopen(MOUNT_POINT"/profiles/foo.txt", "r");
-    if (f == NULL) {
-        ESP_LOGE(TAG, "Failed to open file for reading");
-        return;
-    }
-    char line[64];
-    fgets(line, sizeof(line), f);
-    fclose(f);
-    // strip newline
-    char* pos = strchr(line, '\n');
-    if (pos) {
-        *pos = '\0';
-    }
-    ESP_LOGI(TAG, "Read from file: '%s'", line);
-
     // All done, unmount partition and disable SDMMC or SPI peripheral
-    esp_vfs_fat_sdcard_unmount(mount_point, card);
+    /*esp_vfs_fat_sdcard_unmount(mount_point, card);
     ESP_LOGI(TAG, "Card unmounted");
 #ifdef USE_SPI_MODE
     //deinitialize the bus after all devices are removed
